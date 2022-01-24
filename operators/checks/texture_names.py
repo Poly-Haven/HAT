4e08f76d74f2d118415ec04b42ceef178a348129
Greg Zaal <gregzzmail@gmail.com>
2022-01-24 15:17:02 +0200

    Check texture names are standardized

--- operators/checks/texture_names.py
+++ operators/checks/texture_names.py
@@ -6,11 +6,24 @@ def check(slug):
     result = "SUCCESS"
     messages = []
 
+    allowed_characters = "qwertyuiopasdfghjklzxcvbnm_-0123456789."
+
     textures = fetch_textures()
     for image in textures:
         fn = bpy.path.basename(image.filepath)
         if not fn.startswith(slug):
             result = 'WARNING'
             messages.append(fn + " does not start with slug")
+        if fn.lower() != fn:
+            result = 'WARNING'
+            messages.append(fn + " contains uppercase characters")
+        illegal_characters = []
+        for char in fn:
+            if char.lower() not in allowed_characters:
+                illegal_characters.append(char)
+        if illegal_characters:
+            result = 'WARNING'
+            messages.append(
+                f'{fn} contains illegal characters ({"".join(illegal_characters)})')
 
     return result, messages

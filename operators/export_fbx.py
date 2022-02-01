975406fbf991cec7340c84f679c451731b1e1c23
Greg Zaal <gregzzmail@gmail.com>
2022-02-01 11:23:25 +0200

    Add (unused) FBX export, maybe we need this in future.

---
+++ operators/export_fbx.py
@@ -0,0 +1,45 @@
+import bpy
+import os
+
+
+class HAT_OT_export_fbx(bpy.types.Operator):
+    bl_idname = "hat.export_fbx"
+    bl_label = "Export FBX"
+    bl_description = "Export this asset as a FBX file as standard for uploading to polyhaven.com"
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.data.is_saved and context.scene.hat_props.asset_type == 'model'
+
+    def execute(cls, context):
+        slug = bpy.path.display_name_from_filepath(bpy.data.filepath)
+        to_file = os.path.join(os.path.dirname(
+            bpy.data.filepath), slug + '.fbx')
+
+        try:
+            collection = bpy.data.collections[slug]
+        except KeyError:
+            cls.report({'ERROR'}, "No collection named " + slug)
+            return {'CANCELLED'}
+
+        for o in bpy.data.objects:
+            o.select_set(o in list(collection.objects))
+
+        # Correct and collect image file paths
+        for img in bpy.data.images:
+            if img.filepath.lower().startswith("//textures"):
+                print("INCLUDE_FILE:", img.filepath[2:].replace('\\', '/'))
+                img.filepath = os.path.join(
+                    os.path.dirname(to_file),
+                    img.filepath[len("//"):]
+                ).replace('\\', '/')
+
+        # Export FBX
+        bpy.ops.export_scene.fbx(
+            filepath=to_file,
+            path_mode='RELATIVE',
+            check_existing=False,
+            use_selection=True,
+        )
+
+        return {'FINISHED'}

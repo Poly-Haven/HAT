ad24726ae3f0348c9ea846c7109a04ed554aa6f7
Greg Zaal <gregzzmail@gmail.com>
2024-04-30 13:44:38 +0200

    Tweak non-color check to also check for sRGB maps

--- operators/checks/non_color_data.py
+++ operators/checks/non_color_data.py
@@ -12,15 +12,23 @@ def check(slug):
         "nor_gl",
         "disp",
     ]
+    sRGB_types = [
+        "diff",
+    ]
     aliases = {
-        'roughness': 'rough',
-        'metallic': 'metal',
-        'nor': 'nor_gl',
-        'norm': 'nor_gl',
-        'normal': 'nor_gl',
-        'normals': 'nor_gl',
-        'height': 'disp',
-        'displacement': 'disp',
+        "roughness": "rough",
+        "metallic": "metal",
+        "nor": "nor_gl",
+        "norm": "nor_gl",
+        "normal": "nor_gl",
+        "normals": "nor_gl",
+        "height": "disp",
+        "displacement": "disp",
+        "color": "diff",
+        "col": "diff",
+        "diffuse": "diff",
+        "diffuse_color": "diff",
+        "albedo": "diff",
     }
 
     for img in bpy.data.images:
@@ -30,12 +38,16 @@ def check(slug):
                 map_name = aliases[map_name]
 
             if map_name in linear_types:
-                if img.colorspace_settings.name != 'Non-Color':
-                    result = 'ERROR'
-                    messages.append(bpy.path.basename(
-                        img.filepath) + " isn't Non-Color")
+                if img.colorspace_settings.name != "Non-Color":
+                    result = "ERROR"
+                    messages.append(bpy.path.basename(img.filepath) + " isn't Non-Color")
+
+            if map_name in sRGB_types:
+                if img.colorspace_settings.name != "sRGB":
+                    result = "ERROR"
+                    messages.append(bpy.path.basename(img.filepath) + " isn't sRGB")
 
     if result == "SUCCESS":
-        messages = ["All data texture maps are Non-Color"]
+        messages = ["All data texture maps are in the correct color space"]
 
     return result, messages

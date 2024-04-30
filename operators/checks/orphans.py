b3f3fbec9a820bbb971c21f806c2e051c5c27762
Greg Zaal <gregzzmail@gmail.com>
2024-04-30 13:49:18 +0200

    Fix "Unused imag: ..." message to "Unused images: ..."

--- operators/checks/orphans.py
+++ operators/checks/orphans.py
@@ -7,19 +7,14 @@ def check(slug):
     messages = []
 
     for data_type in dir(bpy.data):
-        if hasattr(getattr(bpy.data, data_type), '__iter__'):
+        if hasattr(getattr(bpy.data, data_type), "__iter__"):
             l = list(getattr(bpy.data, data_type))
             for item in l:
-                if not hasattr(item, 'users'):
+                if not hasattr(item, "users"):
                     continue
                 if item.users == 0:
-                    dt_name = (data_type[:-2]
-                               if data_type.endswith('es')
-                               else data_type[:-1]
-                               if data_type.endswith('s')
-                               else data_type).replace('_', ' ')
-                    result = 'ERROR'
-                    messages.append(
-                        f'Unused {dt_name}: {item.name}')
+                    dt_name = data_type.replace("_", " ")
+                    result = "ERROR"
+                    messages.append(f"Unused {dt_name}: {item.name}")
 
     return result, messages

2a36349aba460cf0a397db683283a2db96785384
Greg Zaal <gregzzmail@gmail.com>
2025-05-28 13:03:54 +0200

    Echo some build instructions

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
+@echo off
 rm index.json
 blender --factory-startup --command extension build
 blender --factory-startup --command extension server-generate --repo-dir=.
+
+echo Now fix index.json, ensure URL is correct
+echo Create the release on GH and test the archive URL

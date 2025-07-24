c821eb648b1c25961e2adad7dc6b014d95f460e8
Greg Zaal <gregzzmail@gmail.com>
2025-07-24 15:35:45 +0200

    Check all image resolutions match

---
+++ operators/checks/resolutions_match.py
@@ -0,0 +1,20 @@
+from ...utils.fetch_textures import fetch_textures
+
+
+def check(slug):
+    result = "SUCCESS"
+    messages = []
+
+    resolutions = {}
+
+    textures = fetch_textures()
+    for image in textures:
+        res_str = f"{image.size[0]}x{image.size[1]}"
+        resolutions[res_str] = resolutions.get(res_str, []) + [image]
+
+    if len(resolutions) > 1:
+        result = "ERROR"
+        for res_str, images in resolutions.items():
+            messages.append(f"Resolution {res_str} has {len(images)} images")
+
+    return result, messages

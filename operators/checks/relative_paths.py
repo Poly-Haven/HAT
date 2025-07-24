e3af0665f224316e4f496571154c6ea9a2e77dc3
Greg Zaal <gregzzmail@gmail.com>
2025-07-24 15:23:15 +0200

    Check Tweak relative path check

--- operators/checks/relative_paths.py
+++ operators/checks/relative_paths.py
@@ -1,3 +1,4 @@
+import bpy
 from ...utils.fetch_textures import fetch_textures
 
 
@@ -7,8 +8,8 @@ def check(slug):
 
     textures = fetch_textures()
     for image in textures:
-        if not image.filepath.startswith("//textures"):
+        if not image.filepath.replace("\\", "/").startswith("//textures/"):
             result = "ERROR"
-            messages.append(f"{image.name} path doesn't start with //textures")
+            messages.append(f"{bpy.path.basename(image.filepath)} is not in the textures folder")
 
     return result, messages

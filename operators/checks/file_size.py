e46db2d68dc82c4bf9546ddd6fcf2dc43db37fbc
Greg Zaal <gregzzmail@gmail.com>
2025-07-25 14:38:54 +0200

    Check file size limit

---
+++ operators/checks/file_size.py
@@ -0,0 +1,27 @@
+import bpy
+import os
+
+
+def check(slug):
+    result = "SUCCESS"
+    messages = []
+
+    if bpy.data.filepath:
+        file_size = os.path.getsize(bpy.data.filepath)
+
+        if bpy.context.scene.hat_props.asset_type == "texture":
+            warn_file_size = 300  # kB
+            max_file_size = 500
+        else:
+            warn_file_size = 10 * 1024
+            max_file_size = 100 * 1024
+
+        if file_size > max_file_size * 1024:
+            result = "ERROR"
+            messages.append(f"File size >{max_file_size}kB, unnecessary data present?")
+            return result, messages
+        elif file_size > warn_file_size * 1024:
+            result = "WARNING"
+            messages.append(f"File size >{warn_file_size}kB")
+
+    return result, messages

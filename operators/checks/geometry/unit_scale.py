ce0c4240961b4f54765a124a34f2a586df7a286e
Greg Zaal <gregzzmail@gmail.com>
2025-08-07 14:50:14 +0200

    We don't actually care if it's not meters, any metric unit is fine.

--- operators/checks/geometry/unit_scale.py
+++ operators/checks/geometry/unit_scale.py
@@ -2,17 +2,13 @@ import bpy
 
 
 def check(slug):
-    """Scene unit scale is set to 1.0 meters"""
+    """Scene unit scale is set to 1.0 and is Metric"""
     result = "SUCCESS"
     messages = []
 
     if bpy.context.scene.unit_settings.scale_length != 1:
         result = "ERROR"
         messages.append("Scene Unit Scale is not 1.0")
-
-    if bpy.context.scene.unit_settings.length_unit != "METERS":
-        result = "ERROR"
-        messages.append("Scene Unit Length is not Meters")
 
     if bpy.context.scene.unit_settings.system != "METRIC":
         result = "ERROR"

8be8279f0cfcccd22782753d89d60110f4e0ec45
Greg Zaal <gregzzmail@gmail.com>
2025-08-07 14:55:44 +0200

    Reduce spam if many objects not at origin

--- operators/checks/geometry/object_origin.py
+++ operators/checks/geometry/object_origin.py
@@ -9,10 +9,21 @@ def check(slug):
 
     zero = Vector((0, 0, 0))
 
+    objects = []
+
     for obj in bpy.data.objects:
         if obj.type == "MESH":
             if obj.location != zero:
                 result = "WARNING" if bpy.context.window_manager.hat_props.asset_type == "texture" else "QUESTION"
-                messages.append(obj.name + " is not at origin")
+                objects.append(obj)
+
+    if not objects:
+        return result, messages
+
+    if len(objects) <= 5:
+        for obj in objects:
+            messages.append(f"{obj.name} is not at origin")
+    else:
+        messages.append(f"{len(objects)} objects are not at origin")
 
     return result, messages
